8d31c38f063599189527733a3e40a7ffa4bd7237
YoanTLS <yoanperes31@gmail.com>
2024-08-08 18:34:58 +0200

    Update printer.py

--- printer.py
+++ printer.py
@@ -4,9 +4,10 @@ from scapy.layers.inet import UDP, IP
 from scapy.layers.l2 import Ether
 
 
-client_mac = "00:11:22:33:44:55" 
+client_mac = "00:11:22:33:44:55"  
 client_ip = "0.0.0.0"
 server_ip = "255.255.255.255"
+
 
 ethernet = Ether(dst="ff:ff:ff:ff:ff:ff", src=client_mac, type=0x0800)
 ip = IP(src=client_ip, dst=server_ip)
@@ -15,11 +16,11 @@ bootp = BOOTP(chaddr=[mac2str(client_mac)], xid=RandInt(), flags=0x8000)
 dhcp = DHCP(options=[
     ("message-type", "discover"),
     ("param_req_list", [1, 3, 6, 12, 15, 28, 42, 51, 54, 58, 59]),
-    ("vendor_class_id", "Printer"),  //Change this
-    ("client_identifier", client_mac), 
+    ("vendor_class_id", "Toshiba"),  # Toshiba for exemple
+    ("client_identifier", client_mac),
     "end"
 ])
 
 dhcp_discover = ethernet / ip / udp / bootp / dhcp
 
-sendp(dhcp_discover, iface="eth0")  # Remplacez "eth0" par l'interface réseau appropriée
+sendp(dhcp_discover, iface="eth0")
